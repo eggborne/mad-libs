09fcb6be11c845aecf34af23a7812d38b55e10d2
Eric Conner and Michael Donovan <michael@>
2018-08-08 10:17:31 -0700

    Add cool transition effect

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
     var adjInput2 = $("input#adj-input-2").val();
 
     var story = nameInput1 + " was the son of "+nameInput2+", a laborer, and Aretha (or Reatha) Williams. At the time, his mother was a "+adjInput1+" orphan "+verbInput2+" a living as a "+nounInput2+" driver. They lived in "+nounInput1+", Florida with Robinson's father and his wife, Mary Jane Robinson. The Robinson family had informally "+verbInput1+" Aretha (or Reatha), and she took the "+adjInput2+" surname Robinson."
-
-
     $('#output').append(story)
+    $('#output').css({
+      'transform':'scaleX(1) scaleY(1)',
+      'opacity':'1'
+    })
     event.preventDefault();
   });
 });
